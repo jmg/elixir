2ccafd51b09dc715be6fbe2ca2960aa0799dd94a
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-02-28 09:28:54 +0000

    release time for setup.py too :)
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@78 6b2338c1-e826-0410-b488-bc38001cd02a

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="Elixir",
-      version="0.1.0",
+      version="0.2.0",
       description="Declarative Mapper for SQLAlchemy",
       long_description="""
 Elixir
