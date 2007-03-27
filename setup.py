6a16fa79a6be1ec43cad3c95c5f7e5dda17eb534
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-03-27 15:37:45 +0000

    prepare for release
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@99 6b2338c1-e826-0410-b488-bc38001cd02a

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="Elixir",
-      version="0.2.0",
+      version="0.3.0",
       description="Declarative Mapper for SQLAlchemy",
       long_description="""
 Elixir
