b7efe22f520fc805d148a49eb71c12b445ba9281
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-06-28 08:45:02 +0000

    bump version number to 0.4.0
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@137 6b2338c1-e826-0410-b488-bc38001cd02a

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="Elixir",
-      version="0.3.1",
+      version="0.4.0",
       description="Declarative Mapper for SQLAlchemy",
       long_description="""
 Elixir
