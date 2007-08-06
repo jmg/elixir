5c6cdaef63827026bcf6b326706a8df6d38e509a
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-08-06 12:18:30 +0000

    minor cleanup to event code
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@177 6b2338c1-e826-0410-b488-bc38001cd02a

--- elixir/events.py
+++ elixir/events.py
@@ -1,6 +1,11 @@
-from sqlalchemy.orm.mapper import MapperExtension
-from elixir.statements import Statement
-
+__all__ = [
+    'before_insert',
+    'after_insert',
+    'before_update',
+    'after_update',
+    'before_delete',
+    'after_delete'
+]
 
 def create_decorator(event_name):
     def decorator(func):
@@ -10,7 +15,6 @@ def create_decorator(event_name):
         return func
     return decorator
 
-
 before_insert = create_decorator('before_insert')
 after_insert = create_decorator('after_insert')
 before_update = create_decorator('before_update')
@@ -19,11 +23,4 @@ before_delete = create_decorator('before_delete')
 after_delete = create_decorator('after_delete')
 
 
-__all__ = [
-    'before_insert',
-    'after_insert',
-    'before_update',
-    'after_update',
-    'before_delete',
-    'after_delete'
-]
+
