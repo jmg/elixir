b5defb05642c51af12408875d36bb254e6239cb8
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-08-06 12:17:32 +0000

    merge, part 2
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@176 6b2338c1-e826-0410-b488-bc38001cd02a

---
+++ tests/test_autodelay.py
@@ -0,0 +1,52 @@
+"""
+    simple test case
+"""
+
+from sqlalchemy import Table
+from elixir import *
+
+
+def setup():
+    metadata.connect('sqlite:///')
+
+def teardown():
+    cleanup_all()
+
+class TestAutoDelay(object):
+    def teardown(self):
+        cleanup_all()
+    
+    def test_call(self):
+        class Person(Entity):
+            has_field('name', Unicode(30))
+            using_options(tablename='person')
+
+        assert 'person' in metadata.tables
+        homer = Person(name="Homer Simpson")
+        assert isinstance(metadata.tables['person'], Table)
+        
+    def test_getattr(self):
+        class Person(Entity):
+            has_field('name', Unicode(30))
+            using_options(tablename='person')
+
+        tablename = Person.table.name
+        assert tablename == 'person' 
+        assert isinstance(metadata.tables['person'], Table)
+
+    def test_createall(self):
+        class Person(Entity):
+            has_field('name', Unicode(30))
+            using_options(tablename='person')
+
+        create_all()
+        assert isinstance(metadata.tables['person'], Table)
+
+    def test_setupall(self):
+        class Person(Entity):
+            has_field('name', Unicode(30))
+            using_options(tablename='person')
+
+        setup_all()
+        assert isinstance(metadata.tables['person'], Table)
+
