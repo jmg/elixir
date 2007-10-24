f58263d3d1a8408e040fa4329bd3c03c7f52c80e
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-10-24 07:46:00 +0000

    added test for sa integration
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@241 6b2338c1-e826-0410-b488-bc38001cd02a

---
+++ tests/test_sa_integeration.py
@@ -0,0 +1,80 @@
+"""
+test integrating Elixir entities with plain SQLAlchemy defined classes
+"""
+
+from sqlalchemy.orm import *
+from sqlalchemy import *
+from elixir import *
+
+def setup():
+    metadata.bind = 'sqlite:///'
+
+class TestSAIntegration(object):
+    def teardown(self):
+        cleanup_all(True)
+    
+    def test_sa_to_elixir(self):
+        class A(Entity):
+            name = Field(Unicode(60))
+
+        # Remember the entity need to be setup before you can refer to it from
+        # SQLAlchemy.
+        setup_all(True)
+
+        b_table = Table('b', metadata, 
+            Column('id', Integer, primary_key=True),
+            Column('name', Unicode(60)),
+            Column('a_id', Integer, ForeignKey(A.c.id))
+        )
+        b_table.create()
+
+        class B(object):
+            pass
+
+        mapper(B, b_table, properties={
+            'a': relation(A)
+        })
+
+        b1 = B()
+        b1.name = 'b1'
+        b1.a = A(name='a1')
+
+        session.save(b1)
+        session.flush()
+        session.clear()
+
+        b = session.query(B).one()
+
+        assert b.a.name == 'a1'
+
+#    def test_elxir_to_sa(self):
+#        a_table = Table('a', metadata, 
+#            Column('id', Integer, primary_key=True),
+#            Column('name', Unicode(60)),
+#        )
+#        a_table.create()
+#
+#        class A(object):
+#            pass
+#
+#        mapper(A, a_table)
+#
+#        class B(Entity):
+#            name = Field(Unicode(60))
+#            a = ManyToOne('A')
+#    
+#        setup_all(True)
+#
+#        a1 = A()
+#        a1.name = 'a1'
+#        b1 = B(name='b1', a=a1)
+#
+#        session.save(b1)
+#        session.flush()
+#        session.clear()
+#
+#        b = B.query.one()
+#
+#        assert b.a.name == 'a1'
+
+
