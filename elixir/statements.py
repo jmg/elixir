921aef6d67cfc2bea2c17eb802d4bdeec8e52dd3
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2007-12-17 10:58:38 +0000

    changed statement processing slightly so that a child entity doesn't inherit it's parent entity statements (such as options) if it didn't use any statement itself.
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@283 6b2338c1-e826-0410-b488-bc38001cd02a

--- elixir/statements.py
+++ elixir/statements.py
@@ -41,7 +41,9 @@ def process_mutators(entity):
     Apply all mutators of the given entity. That is, loop over all mutators 
     in the class's mutator list and process them.
     '''
-    mutators = getattr(entity, MUTATORS, [])
+    # we don't use getattr here to not inherit from the parent mutators
+    # inadvertantly if the current entity hasn't defined any mutator.
+    mutators = entity.__dict__.get(MUTATORS, [])
     for mutator, args, kwargs in mutators:
         mutator.process(entity, *args, **kwargs)
 
