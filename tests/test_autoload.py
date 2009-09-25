156120314e8ebcf0e962eeb5b467b69de5789722
ged <ged@6b2338c1-e826-0410-b488-bc38001cd02a>
2009-09-25 16:11:59 +0000

    rewrote all autoload tests so that they pass on SA 0.6 and they are not as much interdependant
    git-svn-id: http://elixir.ematia.de/svn/elixir/trunk@470 6b2338c1-e826-0410-b488-bc38001cd02a

--- tests/test_autoload.py
+++ tests/test_autoload.py
@@ -2,7 +2,7 @@
 test autoloaded entities
 """
 
-from sqlalchemy import Table, Column, ForeignKey, MetaData
+from sqlalchemy import Table, Column, ForeignKey
 from elixir import *
 import elixir
 
@@ -17,80 +17,43 @@ def setup_entity_raise(cls):
 # ------
 
 def setup():
-    # First create the tables
-    meta = MetaData('sqlite://')
-
-    person_table = Table('person', meta,
-        Column('id', Integer, primary_key=True),
-        Column('father_id', Integer, ForeignKey('person.id')),
-        Column('name', String(32)))
-
-    animal_table = Table('animal', meta,
-        Column('id', Integer, primary_key=True),
-        Column('name', String(30)),
-        Column('owner_id', Integer, ForeignKey('person.id')),
-        Column('feeder_id', Integer, ForeignKey('person.id')))
-
-    category_table = Table('category', meta,
-        Column('name', String(30), primary_key=True))
-
-    person_category_table = Table('person_category', meta,
-        Column('person_id', Integer, ForeignKey('person.id')),
-        Column('category_name', String(30), ForeignKey('category.name')))
-
-    person_person_table = Table('person_person', meta,
-        Column('person_id1', Integer, ForeignKey('person.id')),
-        Column('person_id2', Integer, ForeignKey('person.id')))
-
-    meta.create_all()
-
     elixir.options_defaults.update(dict(autoload=True, shortnames=True))
 
-    global Person, Animal, Category
-
-    #TODO: split these into individual classes for each test.
-    class Person(Entity):
-        father = ManyToOne('Person')
-        children = OneToMany('Person')
-        pets = OneToMany('Animal', inverse='owner')
-        animals = OneToMany('Animal', inverse='feeder')
-        categories = ManyToMany('Category',
-                                tablename='person_category')
-        appreciate = ManyToMany('Person',
-                                tablename='person_person',
-                                local_colname='person_id1')
-        isappreciatedby = ManyToMany('Person',
-                                tablename='person_person',
-                                local_colname='person_id2')
-
-    class Animal(Entity):
-        owner = ManyToOne('Person', colname='owner_id')
-        feeder = ManyToOne('Person', colname='feeder_id')
-
-
-    class Category(Entity):
-        persons = ManyToMany('Person',
-                                tablename='person_category')
-
-    metadata.bind = meta.bind
-    setup_all()
-
-
 def teardown():
-    cleanup_all()
     elixir.options_defaults.update(dict(autoload=False, shortnames=False))
 
 # -----------
 
 class TestAutoload(object):
     def setup(self):
-        create_all()
+        metadata.bind = 'sqlite://'
 
     def teardown(self):
-        drop_all()
-        session.clear()
+        cleanup_all(True)
 
     def test_simple(self):
+        person_table = Table('person', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(32)))
+
+        animal_table = Table('animal', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(30)),
+            Column('owner_id', Integer, ForeignKey('person.id')),
+            Column('feeder_id', Integer, ForeignKey('person.id')))
+
+        metadata.create_all()
+
+        class Person(Entity):
+            pets = OneToMany('Animal', inverse='owner')
+            animals = OneToMany('Animal', inverse='feeder')
+
+        class Animal(Entity):
+            owner = ManyToOne('Person', colname='owner_id')
+            feeder = ManyToOne('Person', colname='feeder_id')
+
+        setup_all()
+
         snowball = Animal(name="Snowball II")
         slh = Animal(name="Santa's Little Helper")
         homer = Person(name="Homer", animals=[snowball, slh], pets=[slh])
@@ -108,6 +71,18 @@ class TestAutoload(object):
         assert homer == slh.owner
 
     def test_selfref(self):
+        person_table = Table('person', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('father_id', Integer, ForeignKey('person.id')),
+            Column('name', String(32)))
+        metadata.create_all()
+
+        class Person(Entity):
+            father = ManyToOne('Person')
+            children = OneToMany('Person')
+
+        setup_all()
+
         grampa = Person(name="Abe")
         homer = Person(name="Homer")
         bart = Person(name="Bart")
@@ -128,6 +103,29 @@ class TestAutoload(object):
         assert p is Person.get_by(name="Lisa").father
 
     def test_m2m(self):
+        person_table = Table('person', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(32)))
+
+        category_table = Table('category', metadata,
+            Column('name', String(30), primary_key=True))
+
+        person_category_table = Table('person_category', metadata,
+            Column('person_id', Integer, ForeignKey('person.id')),
+            Column('category_name', String(30), ForeignKey('category.name')))
+
+        metadata.create_all()
+
+        class Person(Entity):
+            categories = ManyToMany('Category',
+                                    tablename='person_category')
+
+        class Category(Entity):
+            persons = ManyToMany('Person',
+                                 tablename='person_category')
+
+        setup_all()
+
         stupid = Category(name="Stupid")
         simpson = Category(name="Simpson")
         old = Category(name="Old")
@@ -149,6 +147,26 @@ class TestAutoload(object):
         assert c in grampa.categories
 
     def test_m2m_selfref(self):
+        person_table = Table('person', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(32)))
+
+        person_person_table = Table('person_person', metadata,
+            Column('person_id1', Integer, ForeignKey('person.id')),
+            Column('person_id2', Integer, ForeignKey('person.id')))
+
+        metadata.create_all()
+
+        class Person(Entity):
+            appreciate = ManyToMany('Person',
+                                    tablename='person_person',
+                                    local_colname='person_id1')
+            isappreciatedby = ManyToMany('Person',
+                                         tablename='person_person',
+                                         local_colname='person_id2')
+
+        setup_all()
+
         barney = Person(name="Barney")
         homer = Person(name="Homer", appreciate=[barney])
 
@@ -164,48 +182,58 @@ class TestAutoload(object):
     # ----------------
     # overrides tests
     # ----------------
+    def _create_table_a(self):
+        a_table = Table('a', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(32)))
+
+        metadata.create_all()
 
     def test_override_pk_fails(self):
-        class Person(Entity):
+        self._create_table_a()
+
+        class A(Entity):
             id = Field(Integer, primary_key=True)
 
-        setup_entity_raise(Person)
+        setup_entity_raise(A)
 
     def test_override_non_pk_fails(self):
-        class Animal(Entity):
+        self._create_table_a()
+
+        class A(Entity):
             name = Field(String(30))
 
-        setup_entity_raise(Animal)
+        setup_entity_raise(A)
 
     def test_override_pk(self):
-        class Person(Entity):
+        self._create_table_a()
+
+        class A(Entity):
             using_options(allowcoloverride=True)
 
             id = Field(Integer, primary_key=True)
 
-        setup_entities([Person])
+        setup_entities([A])
 
     def test_override_non_pk(self):
-        class Animal(Entity):
+        self._create_table_a()
+
+        class A(Entity):
             using_options(allowcoloverride=True)
 
             name = Field(String(30))
 
-        setup_entities([Animal])
-        assert isinstance(Animal.table.columns['name'].type, String)
+        setup_entities([A])
+        assert isinstance(A.table.columns['name'].type, String)
 
-    # ----------------
+    # ---------------
 
     def test_nopk(self):
-        metadata.bind = 'sqlite://'
-
-        local_meta = MetaData(metadata.bind)
-
-        table = Table('a', local_meta,
+        table = Table('a', metadata,
             Column('id', Integer),
             Column('name', String(32)))
 
-        local_meta.create_all()
+        metadata.create_all()
 
         class A(Entity):
             using_mapper_options(primary_key=['id'])
@@ -223,15 +251,11 @@ class TestAutoload(object):
         assert res[0].name == "a1"
 
     def test_inheritance(self):
-        metadata.bind = 'sqlite://'
-
-        local_meta = MetaData(metadata.bind)
-
-        table = Table('father', local_meta,
+        table = Table('father', metadata,
             Column('id', Integer, primary_key=True),
             Column('row_type', elixir.options.POLYMORPHIC_COL_TYPE))
 
-        local_meta.create_all()
+        metadata.create_all()
 
         class Father(Entity):
             pass
